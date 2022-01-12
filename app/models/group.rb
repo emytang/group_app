29e12d0b00870613199c9d439789cbdcecd99384
Emy Tang <mantang@berkeley.edu>
2022-01-12 08:35:44 +0000

    wip

---
+++ app/models/group.rb
@@ -0,0 +1,5 @@
+class Group < ApplicationRecord
+    validates_presence_of :name, :email
+    validates_uniqueness_of :name, :email
+    validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+end
