a3a9f30bf57833f13e3ac6068db6d5605217c18f
Emy Tang <mantang@berkeley.edu>
2022-01-12 18:41:15 +0000

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  belongs_to :group
+  # belongs_to :group
   validates_presence_of :first_name, :last_name, :email
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 end
