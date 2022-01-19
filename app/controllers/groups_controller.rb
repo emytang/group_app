73b5ee357cf87682680a35117b83c942cf760c83
Emy Tang <mantang@berkeley.edu>
2022-01-19 04:23:41 +0000

    wip

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,7 +22,8 @@ class GroupsController < ApplicationController
   end
 
   # def revoke_from_group
-  #   @members.find_by(user_id: ).destroy
+  #   mem = UserGroup.find_by(params[:id]) 
+  #   mem.destroy
   #   respond_to do |format|
   #     format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
   #     format.json { head :no_content }
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get "project/new_release" => 'project#new_release', :as => :new_release
   # get 'groups/users'
+  # get 'ajax/:action', to: 'ajax#:action', :defaults => { :format => 'json' }
+
   get '/revoke_from_group', to: 'application#revoke_from_group', as: :revoke_from_group
+  # get '/revoke_from_group', to: 'groups#revoke_from_group', as: :revoke_from_group
 
   root 'groups#index'
 end
