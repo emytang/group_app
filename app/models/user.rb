b6b3b9f98461d410916c18cb2e4921188d9118c2
Emy <mantang@berkeley.edu>
2022-02-04 10:30:40 -0800

    delete user from a group

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :groups, :through => :groups_users
   has_many :user_groups, dependent: :destroy
   has_many :groups, through: :user_groups
+  validates_uniqueness_of :group
   
   
   
