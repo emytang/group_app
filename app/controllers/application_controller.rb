30079adf84faccc5eac503cc2ce777d725a3c235
emytang <43306480+emytang@users.noreply.github.com>
2022-02-08 08:27:04 -0800

    Revert "Add user" (#6)
    This reverts commit 385a0997fffa917ace7d867684c82a5f2a932ba1.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,13 @@ class ApplicationController < ActionController::Base
  #  include ActionController::MimeResponds
 
  def revoke_from_group
-    g = Group.find_by(params[:group_id])
     mem = UserGroup.find_by(params[:id]) 
     mem.destroy
-    redirect_to g, notice: 'User was successfully destroyed in the group.'
-
+    redirect_to groups_path, notice: 'User was successfully destroyed in the group.'
+    # respond_to do |format|
+    #   format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
  end
   
 end
